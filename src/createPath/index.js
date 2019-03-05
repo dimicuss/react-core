422377cbcd17e236c573f6a4a6c8aaacc1c1e351
dimicuss <dimicuss@gmail.com>
2019-03-05 16:24:09 +0300

    #developing

--- src/createPath/index.js
+++ src/createPath/index.js
@@ -3,9 +3,7 @@ import { stringify } from 'query-string';
 
 export default function createPath(path, fields, properties = {}, query = {}) {
   return [
-    fields.reduce((acc, key) => acc
-      .replace(`{${key}}`, properties[key] || ''), path)
-      .replace(/\/$/, ''),
+    fields.reduce((acc, key) => acc.replace(`{${key}}`, properties[key] || ''), path),
     stringify(query),
   ]
     .filter(str => str !== '')
